28010773923c14b803d56bc222bfe1744e8f592e
Ritik <ritikrajbhagat1@gmail.com>
2023-05-27 23:49:53 +0530

    hero section temporarily complete moving on about section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,6 +11,16 @@ const Hero = () => {
          <div className="w-5 h-5 rounded-full bg-[#d70909]"/>
          <div className="w-1 sm:h-80 h-40 red-gradient " />
         </div>
+
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi I'm <span className=" text-[#f23c3c] ">Ritik</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I am a competitive programmer <br className="sm:block hidden"/> and MERN stack web developer
+          </p>
+        </div>
       </div>
     </section>
   );
