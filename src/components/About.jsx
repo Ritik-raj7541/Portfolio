e8a4d496398f588679a497dd69fb24fd0dd5e373
Ritik <ritikrajbhagat1@gmail.com>
2023-06-08 12:12:34 +0530

    part1 of overview is completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { fadeIn, textVariant } from "../utils/motion";
+import { services } from "../constants";
+// import Tilt from 'react-tilt' ;
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm a skilled computer science final year student at Birla Institute of
+        technology Mesra. I'm MERN developer with problem solving skill. I
+        usually solve codeforces, leetcode, gfg etc. I'm quick learner and
+        collaborate closely with others. I also have intrest in devops, app
+        development, AI and ML.
+      </motion.p>
+
+      
+    </>
+  );
+};
+
+export default About;
