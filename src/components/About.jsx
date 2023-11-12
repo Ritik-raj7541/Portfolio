f962afdba8287dfbf241826f6327771e5c1ec96b
Ritik <ritikrajbhagat1@gmail.com>
2023-11-13 03:31:27 +0530

    first commit like changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,30 +1,78 @@
 import React from "react";
-import { motion } from "framer-motion";
 import { styles } from "../styles";
-import { fadeIn, textVariant } from "../utils/motion";
-import { services } from "../constants";
+import a from "../assets/overview/a.jpg";
+import b from "../assets/overview/b.jpg";
+import c from "../assets/overview/c.jpeg";
+import d from "../assets/overview/d.jpeg";
 // import Tilt from 'react-tilt' ;
 
 const About = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
-      </motion.div>
+      <div className="container mx-6 about-main">
+        <h1 className={`${styles.heroSubText} mt-2 text-white-100`}>
+          Overview
+        </h1>
+      </div>
+      <div className="container about-mid">
+        <div className="container about my-10">
+          <a
+            href="https://www.mythrillfiction.com/the-dark-rider"
+            alt="Mythrill"
+            target="_blank"
+          >
+            <div className="card-about">
+              <div className="wrapper">
+                <img
+                  src={a}
+                  className="cover-image"
+                />
+              </div>
+              <img
+                src="https://ggayane.github.io/css-experiments/cards/dark_rider-title.png"
+                className="title"
+              />
+              <img
+                src="https://ggayane.github.io/css-experiments/cards/dark_rider-character.webp"
+                className="character"
+              />
+            </div>
+          </a>
 
-      <motion.p
-        variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
-      >
-        I'm a skilled computer science final year student at Birla Institute of
-        technology Mesra. I'm MERN developer with problem solving skill. I
-        usually solve codeforces, leetcode, gfg etc. I'm quick learner and
-        collaborate closely with others. I also have intrest in devops, app
-        development, AI and ML.
-      </motion.p>
+          <a
+            href="https://www.mythrillfiction.com/force-mage"
+            alt="Mythrill"
+            target="_blank"
+          >
+            <div className="card-about">
+              <div className="wrapper">
+                <img
+                  src={b}
+                  className="cover-image"
+                />
+              </div>
+              <img
+                src="https://ggayane.github.io/css-experiments/cards/force_mage-title.png"
+                className="title"
+              />
+              <img
+                src="https://ggayane.github.io/css-experiments/cards/force_mage-character.webp"
+                className="character"
+              />
+            </div>
+          </a>
+        </div>
+      </div>
 
-      
+      <div className="container about-main">
+        <div className="container mx-5 my-2 overview">
+          I'm a skilled computer science final year student at Birla Institute
+          of technology Mesra. I'm MERN developer with problem solving skill. I
+          usually solve codeforces, leetcode, gfg etc. I'm quick learner and
+          collaborate closely with others. I also have intrest in devops, app
+          development, AI and ML.
+        </div>
+      </div>
     </>
   );
 };
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
-
+import React from "react";
+import { styles } from "../styles";
+import a from "../assets/experience/a.jpg"
+import b from "../assets/experience/b.jpg"
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <>
+      <div className="container experience my-4">
+        <h1 className={`${styles.heroSubText} mt-2 text-white-100`}>
+          Experience
+        </h1>
+      </div>
+      <div className=" my-4 container exp-over">
+        <div className="card-exp">
+        <img className="card-img" src={b} alt="" />
+          <div className="card-body">
+            <span className="bg"></span>
+            <span className="bg"></span>
+            <span className="bg"></span>
+            <div className="content">
+              <h2 className="title-exp">Web Development</h2>
+              <p className="para">
+                Designed and implemented backend of a web application which is used to authenticate students in hostel mess.
+              </p>
+              <h2 className="title-exp">Skills</h2>
+              <p className="para">
+                Node.js <br /> Express.js <br /> Mongoose <br /> JWT
+              </p>
+            </div>
+          </div>
+        </div>
 
-export default Experience
+        <div className="card-exp">
+            <img className="card-img" src={a} alt="" />
+          <div className="card-body">
+            <span className="bg"></span>
+            <span className="bg"></span>
+            <span className="bg"></span>
+            <div className="content">
+            <h2 className="title">Machine Learning</h2>
+              <p className="para">
+                Designed and implemented user interface of Handwritten digit and character recognition system.
+              </p>
+              <h2 className="title">Skills</h2>
+              <p className="para">
+                Python <br /> maths/coordinations <br /> CNN <br /> Tensorflow
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Experience;
